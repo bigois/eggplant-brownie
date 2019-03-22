a578049e6fad0569edd1a46911d485decdf089d5
Guilherme Bigois <guilhermebigois@Guilhermes-Mac.local>
2019-03-21 21:53:39 -0300

    Início do Projeto

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -1,20 +1,25 @@
-//
 //  ViewController.swift
 //  eggplant-brownie
 //
 //  Created by Guilherme Bigois on 20/03/19.
 //  Copyright © 2019 Alura. All rights reserved.
-//
 
+// IMPORTAÇÃO DO KIT DE INTERFACE GRÁFICA
 import UIKit
 
+// CLASSE PARA CONTROLE DA VIEW HERDANDO
+// O CONTROLE DA INTERFACE COM O USUÁRIO
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var nameField      : UITextField!
+    @IBOutlet var happinessField : UITextField!
+    
+    // AÇÃO DO CONSTRUTOR DE INTERFACE AO CLICAR EM "ADD"
+    @IBAction func add() {
+        // VARIÁVEIS
+        let name      = nameField.text
+        let happiness = happinessField.text
+        
+        // IMPRIME O CONTEÚDO COM INTERPOLAÇÃO DE VARIÁVEIS
+        print("Eaten \(name) with happiness \(happiness)!")
     }
-
-
 }
-
